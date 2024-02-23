1b0c379ddc685c12e0d2d3723773fd10a1ae311d
Francisco Nicolas Hernandorena <fnhernandorena@gmail.com>
2024-02-23 02:18:43 -0300

    dds

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Example02Component } from './pages/example02/example02.component';
 import { Example03Component } from './pages/example03/example03.component';
 import { Example04Component } from './pages/example04/example04.component';
 import { Example05Component } from './pages/example05/example05.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomePageComponent},
@@ -12,5 +13,7 @@ export const routes: Routes = [
     {path: 'example02', component: Example02Component},
     {path: 'example03', component: Example03Component},
     {path: 'example04', component: Example04Component},
-    {path: 'example05', component: Example05Component}  
+    {path: 'example05', component: Example05Component},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: '**', component: NotFoundComponent}
 ];
